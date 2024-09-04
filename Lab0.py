365d912345ae53aa0ee415ef97ad88cef0c06294
pronan <pronan@loyola.edu>
2024-09-04 16:13:56 -0400

    Paige wrote about Theresa

---
+++ Lab0.py
@@ -0,0 +1,7 @@
+print ("CS151 Lab 0")
+print ("Theresa")
+print ("She likes the beach")
+print ("She does club running")
+print ("She's from New Jersey")
+print ("She has a dog named Roxy")
+print ("She has red hair")
