fe02301949debab6095871c44b0ead930510f67e
tadejacimo <tadejacimo@loyola.edu>
2024-09-11 16:21:41 -0400

    Theresa wrote about Paige.

--- Lab0.py
+++ Lab0.py
@@ -1,3 +1,5 @@
+#Paige wrote six sentences about Theresa
+
 print ("CS151 Lab 0")
 print ("Theresa")
 print ("She likes the beach")
@@ -5,4 +7,13 @@ print ("She does club running")
 print ("She's from New Jersey")
 print ("She has a dog named Roxy")
 print ("She has red hair")
-\
+
+
+#Theresa wrote six sentences about Paige
+
+print ("Paige")
+print ("She is from New York")
+print ("Paige has two dogs that are both muts named Moxie and Theo")
+print ("She is the youngest of three daughters")
+print ("She likes to play lacrosse in the spring")
+print ("She likes to read all types of books")
