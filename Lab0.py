c6c8ab0185b0257819c998f4ff7acdadd5b061c4
pronan <pronan@loyola.edu>
2024-09-11 16:25:45 -0400

    Wrote comments and finished

--- Lab0.py
+++ Lab0.py
@@ -1,5 +1,13 @@
-#Paige wrote six sentences about Theresa
+# Programmers: Paige and Theresa
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 9/12/24
+# Lab Assignment: 0
+# Problem Statement:  Output information about the programmers
+# Data In: None, there is no user input in this program
+# Data Out:  The names of the programmers and five statements about each of them
+# Credits: Paige and Theresa
 
+#Paige wrote six sentences about Theresa
 print ("CS151 Lab 0")
 print ("Theresa")
 print ("She likes the beach")
@@ -8,9 +16,7 @@ print ("She's from New Jersey")
 print ("She has a dog named Roxy")
 print ("She has red hair")
 
-
 #Theresa wrote six sentences about Paige
-
 print ("Paige")
 print ("She is from New York")
 print ("Paige has two dogs that are both muts named Moxie and Theo")
